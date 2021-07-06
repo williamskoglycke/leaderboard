9f351227ed68a44e75c640e295dabfc5f80b1376
William Skoglycke <william@skoglycke.se>
2021-07-06 18:05:15 +0200

    Refactored LeaderboardService

--- src/main/java/com/gloot/springbootcodetest/leaderboard/domain/LeaderboardService.java
+++ src/main/java/com/gloot/springbootcodetest/leaderboard/domain/LeaderboardService.java
@@ -10,13 +10,13 @@ import com.gloot.springbootcodetest.leaderboard.infrastructure.entities.Leaderbo
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.IntStream;
 
 import static com.gloot.springbootcodetest.leaderboard.infrastructure.mapper.LeaderboardEntryMapper.mapToDto;
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
 
 @Service
 @AllArgsConstructor
@@ -28,32 +28,14 @@ public class LeaderboardService {
 
 
     public List<LeaderboardEntryDto> getListOfAllLeaderboardEntriesAsDTO() {
-        List<LeaderboardEntryEntity> allEntries = leaderboardEntryRepository.findAll();
-        Collections.sort(allEntries, new Comparator<LeaderboardEntryEntity>() {
-            public int compare(LeaderboardEntryEntity e1, LeaderboardEntryEntity e2) {
-                return e2.getScore() - e1.getScore();
-            }
-        });
+        List<LeaderboardEntryEntity> leaderboardEntries = leaderboardEntryRepository.findAll()
+                .stream()
+                .sorted(comparing(LeaderboardEntryEntity::getScore).reversed())
+                .collect(toList());
 
-        LeaderboardEntryEntity[] allEntriesAsEntities = allEntries.toArray(new LeaderboardEntryEntity[]{});
-        LeaderboardEntryDto[] dtoObjects = new LeaderboardEntryDto[allEntriesAsEntities.length];
-
-        for (int i = allEntriesAsEntities.length - 1; i >= 0; i--) {
-            dtoObjects[i] = mapToDto(i + 1, allEntriesAsEntities[i]);
-        }
-
-        List<LeaderboardEntryDto> leaderboardEntryDtos = new ArrayList<>();
-        for (int j = dtoObjects.length - 1; j >= 0; j--) {
-            leaderboardEntryDtos.add(dtoObjects[j]);
-        }
-
-        Collections.sort(leaderboardEntryDtos, new Comparator<LeaderboardEntryDto>() {
-            public int compare(LeaderboardEntryDto e1, LeaderboardEntryDto e2) {
-                return e1.getPosition() - e2.getPosition();
-            }
-        });
-
-        return leaderboardEntryDtos;
+        return IntStream.range(0, leaderboardEntries.size())
+                .mapToObj(index -> mapToDto(index + 1, leaderboardEntries.get(index)))
+                .collect(toList());
     }
 
     public List<Leaderboard> getAllLeaderboards() {
